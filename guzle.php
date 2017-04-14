e08f2cdc6b85a3856a9cbf79695644f7a4c13ad7
Your Name <you@example.com>
2017-04-14 21:00:45 +0200

    size

--- guzle.php
+++ guzle.php
@@ -1,12 +1,17 @@
 <?php
 use GuzzleHttp\Client;
+require_once 'request.html';
 require 'vendor/autoload.php';
-require
+
 $client = new Client();
-$res = $client->request('GET', ''.$_POST['site'].'/robots.txt');
-echo $res->getStatusCode();
+$res = $client->request('GET', $_POST['site'].'/robots.txt');
 
-echo $res->getHeader('content-type');
+if ($res->getStatusCode() != 404) {
+    $fsize=0;
+    $filename = $_POST['site'].'/robots.txt';
+    $fh = fopen($filename, "r");
+    while(($str = fread($fh, 1024)) != null) $fsize += strlen($str);
 
-echo $res->getBody();
+    echo "Размер файла: ".$fsize. " байт";
 
+}
